fd735b7184bfa7432e11c13004ad1c1804de8e5e
jniac <joseph.merdrignac@gmail.com>
2020-01-08 10:44:13 +0100

    [up] test anim

--- build/graph-svg.module.js
+++ build/graph-svg.module.js
@@ -1,6 +1,6 @@
 /*
 	graph-svg.js
-	2020-01-07 23:44 GMT(+1)
+	2020-01-08 10:43 GMT(+1)
 	js toolkit
 	https://github.com/jniac/js-graph-svg
 */
@@ -16,6 +16,13 @@ class Rectangle {
     set(x, y, width, height) {
 
         Object.assign(this, { x, y, width, height });
+
+    }
+
+    offset(x, y) {
+
+        this.x += x;
+        this.y += y;
 
     }
 
@@ -97,6 +104,7 @@ class Layer {
 
         this.graph = graph;
         this.g = dosvg('g', { id:name });
+        this.objects = [];
 
         graph.svg.insertBefore(this.g, graph.bounds);
 
@@ -108,9 +116,186 @@ class Layer {
 
     }
 
+    draw() {
+
+        this.clear();
+
+        for (const [name, params, props] of this.objects) {
+
+            this[name](...params, props);
+
+        }
+
+    }
+
+    add(name, params, props) {
+
+        this.objects.push([name, params, props]);
+
+    }
+
 }
 
 assignReadonly(Layer, { getLayersProxy });
+
+class Canvas {
+
+    constructor(graph) {
+
+        this.graph = graph;
+
+        const canvas = document.createElement('canvas');
+        graph.wrapper.append(canvas);
+
+        Object.assign(this, { canvas });
+
+    }
+
+    setSize(width, height) {
+
+        const { canvas } = this;
+
+        canvas.width = width;
+        canvas.height = height;
+
+        let gl = canvas.getContext('webgl2');
+
+        if (!gl) {
+            alert('Unable to initialize WebGL. Your browser or machine may not support it.');
+            return;
+        }
+
+        // const shaderProgram = initShaderProgram(gl, vsSource, fsSource)
+
+    }
+}
+
+class Point {
+
+    constructor(x = 0, y = 0) {
+
+        this.set(x, y);
+
+    }
+
+    get components() { return [this.x, this.y] }
+
+    set(x, y) {
+
+        this.x = x;
+        this.y = y;
+
+    }
+
+    copy(other) {
+
+        this.x = other.x;
+        this.y = other.y;
+
+    }
+
+    subVectors(a, b) {
+
+        this.x = a.x - b.x;
+        this.y = a.y - b.y;
+
+    }
+
+}
+
+function onMove(event) {
+
+    const { view, width, height } = this.graph;
+    const { clientX, clientY } = event;
+
+    const bounding = this.graph.wrapper.getBoundingClientRect();
+    const x = clientX - bounding.x;
+    const y = clientY - bounding.y;
+
+    // if first move, initialize this
+    if (!this.hasMoved) {
+
+        this.set(x, y);
+        this.hasMoved = true;
+
+    }
+
+    const { old, delta } = this;
+    old.copy(this);
+    this.set(x, y);
+    delta.subVectors(this, old);
+
+    if (this.down) {
+
+        const dx = -delta.x / width * view.width;
+        const dy = delta.y / height * view.height;
+        view.offset(dx, dy);
+        this.graph.draw();
+
+    }
+
+    this.graph.wrapper.dispatchEvent(new CustomEvent('move'));
+
+}
+
+function onDown(event) {
+
+    this.down = true;
+
+    window.addEventListener('mouseup', this.onUp);
+    this.graph.wrapper.dispatchEvent(new CustomEvent('down'));
+
+}
+
+function onUp(event) {
+
+    this.down = false;
+
+    window.removeEventListener('mouseup', this.onUp);
+    this.graph.wrapper.dispatchEvent(new CustomEvent('up'));
+
+}
+
+class Pointer extends Point {
+
+    constructor(graph) {
+
+        super();
+        this.graph = graph;
+        this.old = new Point();
+        this.delta = new Point();
+
+        this.onMove = onMove.bind(this);
+        this.onDown = onDown.bind(this);
+        this.onUp = onUp.bind(this);
+
+        graph.wrapper.addEventListener('mousemove', this.onMove);
+        graph.wrapper.addEventListener('mousedown', this.onDown);
+
+    }
+
+}
+
+const style = document.createElement('style');
+
+style.innerHTML =
+`
+graph {
+    display:inline-flex;
+    flex-direction:column;
+}
+
+graph div.graph-wrapper {
+    position: relative;
+}
+
+graph div.graph-wrapper svg,
+graph div.graph-wrapper canvas {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+}
+`;
 
 const EPSILON = 1e-6;
 const notNull = x => x < -EPSILON || x > EPSILON;
@@ -220,35 +405,56 @@ class Graph {
 
     constructor(element) {
 
+        const wrapper = document.createElement('div');
+        wrapper.classList.add('graph-wrapper');
+
         const width = 400, height = 400;
-        const svg = dosvg('svg', { width, height });
+        const svg = dosvg('svg', { width, height, parent:wrapper });
         const view = new Rectangle(-4, -4, 8, 8);
-        const layers = Layer.getLayersProxy(this);
+
+        Object.assign(this, { wrapper, svg, view });
 
         const bounds = dosvg('rect', { parent:svg, fill:'none', stroke:'black' });
+        const layers = Layer.getLayersProxy(this);
+        const pointer = new Pointer(this);
+        const canvas = new Canvas(this);
 
-        Object.assign(this, { svg, bounds, view, layers });
+        Object.assign(this, { bounds, layers, pointer, canvas });
 
         this.setSize(width, height);
 
         if (element)
             this.init(element);
 
+        wrapper.addEventListener('mousemove', (e) => {
+
+            let { offsetX:mouseX, offsetY:mouseY } = e;
+            mouseX /= width;
+            mouseY /= height;
+            mouseX = view.localX(mouseX);
+            mouseY = view.localX(1 - mouseY);
+            Object.assign(this, { mouseX, mouseY });
+
+            svg.dispatchEvent(new CustomEvent('move'));
+
+        });
+
     }
 
     setSize(width, height) {
 
-        const { svg, bounds, view } = this;
+        const { wrapper, svg, bounds, view, canvas } = this;
 
         Object.assign(this, { width, height });
         dosvg(svg, { width, height });
         dosvg(bounds, { x:.5, y:.5, width:width - 1, height:height - 1 });
 
-        let { grid: grid$$1 } = this.layers;
-        let step = 1;
+        wrapper.style.width = `${width}px`;
+        wrapper.style.height = `${height}px`;
 
-        grid$$1.clear();
-        grid$$1.grid(step);
+        canvas.setSize(width, height);
+
+        this.draw();
 
     }
 
@@ -278,11 +484,26 @@ class Graph {
             if (blend)
                 props.style = `mix-blend-mode:${blend}`;
 
-            this.layers.main[name](...params, props);
+            this.layers.main.add(name, params, props);
 
         }
 
         element.graph = this;
+
+        this.draw();
+
+    }
+
+    draw() {
+
+        let { grid: grid$$1, ...others } = this.layers;
+        let step = 1;
+
+        grid$$1.clear();
+        grid$$1.grid(step);
+
+        for (let layer of Object.values(others))
+            layer.draw();
 
     }
 
@@ -314,14 +535,12 @@ const parseDocument = (parentElement) => {
     if (!parentElement)
         parentElement = document.body;
 
-    let style = document.createElement('style');
-    style.innerHTML = `graph { display:flex; flex-direction:column; }`;
     document.head.append(style);
 
     for (let element of parentElement.querySelectorAll('graph')) {
 
         let graph = new Graph(element);
-        element.insertAdjacentElement('beforeend', graph.svg);
+        element.insertAdjacentElement('beforeend', graph.wrapper);
 
     }
 
--- test/from-src.js
+++ test/from-src.js
@@ -4,3 +4,16 @@ Graph.parseDocument()
 
 window.Graph = Graph
 window.graph = document.querySelector('graph').graph
+
+function loop(now) {
+
+    const s = now / 1e3
+
+    graph.layers.anim.clear()
+    graph.layers.anim.func(x => .2 * Math.sin(x + s), { stroke:'#00f' })
+
+    requestAnimationFrame(loop)
+
+}
+
+requestAnimationFrame(loop)
